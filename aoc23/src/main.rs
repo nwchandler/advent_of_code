c750b1f31d8b9a8b847fe6c9123350535bba47b2
Nick Chandler <nwchandler@users.noreply.github.com>
2023-12-11 18:40:27 -0500

    Initial solution for day01

---
+++ aoc23/src/main.rs
@@ -0,0 +1,56 @@
+use std::env;
+use std::process;
+
+use aoc23::*;
+
+const ARGUMENT_ERROR: i32 = 1;
+const FUNCTION_ERROR: i32 = 2;
+
+fn main() {
+    // skip the first argument, which is the command name
+    let mut args = env::args().skip(1);
+    let day: usize;
+    match args.next() {
+        Some(i) => {
+            day = i.parse().unwrap_or_else(|err| {
+                eprintln!("{err}");
+                process::exit(ARGUMENT_ERROR);
+            });
+        }
+        None => {
+            println!("must specify the day as a number");
+            process::exit(ARGUMENT_ERROR);
+        }
+    }
+
+    let days = [day01::run];
+
+    if day > days.len() {
+        eprintln!("day {day} is not implemented");
+        process::exit(ARGUMENT_ERROR);
+    }
+    let func = days[day - 1];
+
+    let input = std::io::read_to_string(std::io::stdin()).unwrap_or_else(|err| {
+        eprintln!("{err}");
+        process::exit(ARGUMENT_ERROR);
+    });
+
+    let result = func(&input);
+
+    match result {
+        Ok(output) => {
+            print_result(day, &output);
+        }
+        Err(err) => {
+            eprintln!("error from function: {err}");
+            process::exit(FUNCTION_ERROR);
+        }
+    }
+}
+
+fn print_result(day: usize, solution: &Solution) {
+    println!("-- Day {:02} --", day);
+    println!("   Part 1: {}", solution.part1);
+    println!("   Part 2: {}", solution.part2);
+}
