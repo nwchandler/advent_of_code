c46954fa6763cee23d7942d71852203b88c67606
Nick Chandler <nwchandler@users.noreply.github.com>
2023-12-24 20:56:11 -0500

    day 7 part 2

--- aoc23/src/day07.rs
+++ aoc23/src/day07.rs
@@ -31,7 +31,7 @@ fn part1(input: &str) -> Result<String, &'static str> {
         let bid = line_iter.next().unwrap().parse::<u32>().unwrap();
         let hand_numeric_value = u32::from_str_radix(hand.as_str(), 16).unwrap();
 
-        let category = match categorize_hand(hand.as_str()) {
+        let category = match categorize_hand(hand.as_str(), &None) {
             HandType::FiveOfAKind => 6,
             HandType::FourOfAKind => 5,
             HandType::FullHouse => 4,
@@ -57,7 +57,41 @@ fn part1(input: &str) -> Result<String, &'static str> {
 }
 
 fn part2(input: &str) -> Result<String, &'static str> {
-    let result: u32 = 0;
+    let mut result: u32 = 0;
+
+    let mut categories: Vec<Vec<u32>> =
+        vec![vec![], vec![], vec![], vec![], vec![], vec![], vec![]];
+    let mut hand_to_bid: HashMap<u32, u32> = HashMap::new();
+
+    for line in input.lines() {
+        let mut line_iter = line.trim().split_whitespace();
+        // kind of hacky, but just replace J with 1 to signify its lower individual value
+        let hand = convert_hand(line_iter.next().unwrap().replace('J', "1").as_str());
+        let bid = line_iter.next().unwrap().parse::<u32>().unwrap();
+        let hand_numeric_value = u32::from_str_radix(hand.as_str(), 16).unwrap();
+
+        // because we replaced "J" with "1" up above, 1 is now our joker
+        let category = match categorize_hand(hand.as_str(), &Some('1')) {
+            HandType::FiveOfAKind => 6,
+            HandType::FourOfAKind => 5,
+            HandType::FullHouse => 4,
+            HandType::ThreeOfAKind => 3,
+            HandType::TwoPair => 2,
+            HandType::OnePair => 1,
+            HandType::HighCard => 0,
+        };
+        categories[category].push(hand_numeric_value);
+        hand_to_bid.insert(hand_numeric_value, bid);
+    }
+
+    let mut place = 1;
+    for mut category in categories {
+        category.sort();
+        for hand in category {
+            result += place * hand_to_bid.get(&hand).unwrap();
+            place += 1;
+        }
+    }
 
     Ok(result.to_string())
 }
@@ -73,18 +107,27 @@ fn convert_hand(input: &str) -> String {
         .replace('T', "A")
 }
 
-fn categorize_hand(input: &str) -> HandType {
+fn categorize_hand(input: &str, joker: &Option<char>) -> HandType {
     let trimmed_input = input.trim();
     let mut count_map: HashMap<char, u8> = HashMap::new();
     let mut count_vec: Vec<u8> = vec![0; 5];
 
+    let mut joker_count = 0;
+
     for (i, c) in trimmed_input.chars().enumerate() {
+        if let Some(j) = joker {
+            if &c == j {
+                joker_count += 1;
+                continue;
+            }
+        }
+
         let idx = count_map.entry(c).or_insert(i as u8).to_owned();
         count_vec[idx as usize] += 1;
     }
 
     count_vec.sort();
-    match count_vec[4] {
+    match count_vec[4] + joker_count {
         5 => HandType::FiveOfAKind,
         4 => HandType::FourOfAKind,
         3 => match count_vec[3] {
@@ -129,47 +172,82 @@ mod tests {
 
     #[test]
     fn test_categorize_hand() {
+        // no jokers
+        let mut joker: Option<char> = None;
         {
             // five of a kind
             let input = "AAAAA";
-            let result = categorize_hand(input);
+            let result = categorize_hand(input, &joker);
             assert!(matches!(result, HandType::FiveOfAKind));
         }
         {
             // four of a kind
             let input = "AA8AA";
-            let result = categorize_hand(input);
+            let result = categorize_hand(input, &joker);
             assert!(matches!(result, HandType::FourOfAKind));
         }
         {
             // full house
             let input = "23332";
-            let result = categorize_hand(input);
+            let result = categorize_hand(input, &joker);
             assert!(matches!(result, HandType::FullHouse));
         }
         {
             // three of a kind
             let input = "TTT98";
-            let result = categorize_hand(input);
+            let result = categorize_hand(input, &joker);
             assert!(matches!(result, HandType::ThreeOfAKind));
         }
         {
             // two pair
             let input = "23432";
-            let result = categorize_hand(input);
+            let result = categorize_hand(input, &joker);
             assert!(matches!(result, HandType::TwoPair));
         }
         {
             // one pair
             let input = "A23A4";
-            let result = categorize_hand(input);
+            let result = categorize_hand(input, &joker);
             assert!(matches!(result, HandType::OnePair));
         }
         {
             // high card
             let input = "23456";
-            let result = categorize_hand(input);
+            let result = categorize_hand(input, &joker);
             assert!(matches!(result, HandType::HighCard));
+        }
+
+        // with jokers
+        joker = Some('J');
+        {
+            // five of a kind
+            let input = "JJAAA";
+            let result = categorize_hand(input, &joker);
+            assert!(matches!(result, HandType::FiveOfAKind));
+        }
+        {
+            // four of a kind
+            let input = "AJ8AA";
+            let result = categorize_hand(input, &joker);
+            assert!(matches!(result, HandType::FourOfAKind));
+        }
+        {
+            // full house
+            let input = "2J332";
+            let result = categorize_hand(input, &joker);
+            assert!(matches!(result, HandType::FullHouse));
+        }
+        {
+            // three of a kind
+            let input = "TTJ98";
+            let result = categorize_hand(input, &joker);
+            assert!(matches!(result, HandType::ThreeOfAKind));
+        }
+        {
+            // one pair
+            let input = "A23J4";
+            let result = categorize_hand(input, &joker);
+            assert!(matches!(result, HandType::OnePair));
         }
     }
 
@@ -184,12 +262,14 @@ QQQJA 483";
         assert_eq!(result.unwrap(), "6440");
     }
 
-    // TODO: Don't ignore after implementing
-    #[ignore]
     #[test]
     fn integration_test_part2() {
-        let input = "";
+        let input = "32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483";
         let result = part2(input);
-        assert_eq!(result.unwrap(), "??");
+        assert_eq!(result.unwrap(), "5905");
     }
 }
--- aoc23/src/lib.rs
+++ aoc23/src/lib.rs
@@ -4,8 +4,6 @@ pub mod day03;
 pub mod day04;
 pub mod day05;
 pub mod day06;
-// TODO: Don't allow after implementing
-#[allow(unused_variables, dead_code)]
 pub mod day07;
 
 pub struct Solution {
