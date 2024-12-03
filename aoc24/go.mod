3dbb9e4b8703752d6c64baf3e8a346ae5bbd25a0
Nick Chandler <nwchandler@users.noreply.github.com>
2024-12-03 09:17:54 -0500

    AOC24 Day 1 (#11)

---
+++ aoc24/go.mod
@@ -0,0 +1,3 @@
+module github.com/nwchandler/advent_of_code/aoc24
+
+go 1.23.3
---
+++ aoc24/solve.go
@@ -0,0 +1,21 @@
+package aoc24
+
+import (
+	"fmt"
+	"io"
+)
+
+// Result contains the results of both parts of a day's problem.
+type Result struct {
+	Part1 string
+	Part2 string
+}
+
+// Solve attempts to solve the day's problem, given the provided input.
+func Solve(day int, input io.Reader) (Result, error) {
+	switch day {
+	case 1:
+		return Day01(input)
+	}
+	return Result{}, fmt.Errorf("no solvers found for day %d", day)
+}
